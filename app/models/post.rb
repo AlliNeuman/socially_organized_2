4e53158489e90441a898da804abb0ded3b16179b
Alli Tzadok <allisontzadok@gmail.com>
2018-04-28 15:47:34 -0500

    add associations to post, user_calendar and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_many :calendar_posts
+  has_many :calendars, through: :calendar_posts
+  has_many :platform_posts
+  has_many :platforms, through: :platform_posts
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_calendars
+  has_many :calendars, through: :user_calendars
+  has_many :posts
+
+  has_secure_password
 end
