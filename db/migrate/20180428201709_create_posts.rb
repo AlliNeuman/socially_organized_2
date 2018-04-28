1de5259045a2c69c7b1f121d0526ff147794d75b
Alli Tzadok <allisontzadok@gmail.com>
2018-04-28 15:28:15 -0500

    migrate all database setup

--- db/migrate/20180428201709_create_posts.rb
+++ db/migrate/20180428201709_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :content
       t.string :link
-      t.sting :image_link
+      t.string :image_link
       t.boolean :finalized
       t.integer :user_id
 
