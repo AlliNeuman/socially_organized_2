01ce92d45053078a1b13f15edd434c64ea86ace5
Alli Tzadok <allisontzadok@gmail.com>
2018-04-29 14:44:58 -0500

    add logic and authentication to create method in sessions controller, and logout/destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by_email(params[:email])
+    #if the user exists and password entered is correct
+    if @user && @user.authenticate(params[:password])
+      #save user.id inside the browser cookie
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      #if user's login doesn't work, send them to login with error notice
+      redirect_to login_path, alert: "Something went wrong, please try to login again."
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to login_path, alert: "You have successfully logged out."
   end
 
-end 
+end
